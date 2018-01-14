582a2bfac000cedd323350d831885542d0dac591
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-14 18:53:07 +0000

    Done.

--- if.js
+++ if.js
@@ -4,5 +4,14 @@ if (pokemon == "Pikachu") {
   console.log("I choose you Pikachu!!")
 }
 
+function greaterThan(num){
+  if (num > 10) {
+    return num
+  } else {
+  
+      return "Nope."
+    }
+}
+
 
 
