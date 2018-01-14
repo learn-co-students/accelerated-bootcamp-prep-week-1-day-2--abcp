d4cb97caa10c50f79a012c541085af67fdf6f887
Kristen Bullock <kristenbullock84@gmail.com>
2018-01-14 22:29:54 +0000

    Done.

--- array.js
+++ array.js
@@ -1,17 +1,20 @@
 function addElementToBeginningOfArray(array, element) {
- // add the element to the beginning of the array without mutating the array
+ return [element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   // add element to the beginning of the array by mutating the array
+  array.unshift(element);
+  return array;
 }
 
 function addElementToEndOfArray(array, element) {
-
+  return [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+  array.push(element);
+  return array;
 }
 
 function accessElementInArray(array, index) {
@@ -19,16 +22,20 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-
+  array.shift(elemet);
 }
 
 function removeElementFromBeginningOfArray(array) {
+  newArray = array.slice(0);
+  return newArray;
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-
+  array.pop(element);
+  return array;
 }
 
 function removeElementFromEndOfArray(array) {
-
+  newArray = array.pop(element);
+  return newArray;
 }
--- if.js
+++ if.js
@@ -4,5 +4,19 @@ if (pokemon == "Pikachu") {
   console.log("I choose you Pikachu!!")
 }
 
+function takeANumber(num) {
+  if (num > 10) {
+    return num;
+  } else {
+    return "sorry, wrong number";
+  }
+}
 
+for (i= 0; i < names.length; i++){
+  console.log
+}
+
+// [1, 2, 3].forEach(function(element)){
+//  console.log(name);
+// } 
 
