c1679227b005f0c49d4814a5a54052a29e0ae92d
Tara Johnson <tara.e.johnson@gmail.com>
2018-01-14 18:16:15 +0000

    Done.

--- array.js
+++ array.js
@@ -1,34 +1,42 @@
 function addElementToBeginningOfArray(array, element) {
- // add the element to the beginning of the array without mutating the array
+   return [element, ...array]
+   // add the element to the beginning of the array without mutating the array
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
+  return [array.unshift(element)]
   // add element to the beginning of the array by mutating the array
 }
 
 function addElementToEndOfArray(array, element) {
-
+  return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+  array.push(element)
+  return array
 }
 
 function accessElementInArray(array, index) {
-
+  return array[element]
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-
+  array.shift()
+  return array
 }
 
-function removeElementFromBeginningOfArray(array) {
+function removeElementFromBeginningOfArray(array, element) {
+  array.slice(1)
+  return array
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-
+  array.pop()
+  return array
 }
 
-function removeElementFromEndOfArray(array) {
-
+function removeElementFromEndOfArray(array, element) {
+  array.slice(0, array.length - 1)
+  return array
 }
