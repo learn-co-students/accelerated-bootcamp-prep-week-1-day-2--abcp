c6af9f6374a00446b8c53bb1be060b4f1862fb7c
Hany Rostom <hanyrostom@gmail.com>
2018-01-15 21:56:58 +0000

    Done.

--- array.js
+++ array.js
@@ -1,28 +1,31 @@
 function addElementToBeginningOfArray(array, element) {
  // add the element to the beginning of the array without mutating the array
+ return [element,...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   // add element to the beginning of the array by mutating the array
+  return array.unshift(element);
 }
 
 function addElementToEndOfArray(array, element) {
-
+return [array...,element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+return array.push(element);
 }
 
 function accessElementInArray(array, index) {
-
+return array[index];
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-
+return array.shift();
 }
 
 function removeElementFromBeginningOfArray(array) {
+
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
