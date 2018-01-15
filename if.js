9e79df2aa8a26f92347222b21e3b298e4797d37d
Mike Bakker <equanoxproductions@gmail.com>
2018-01-15 03:42:18 +0000

    Done.

--- if.js
+++ if.js
@@ -3,6 +3,10 @@ var pokemon = "Pikachu"
 if (pokemon == "Pikachu") {
   console.log("I choose you Pikachu!!")
 }
-
-
+function number(num) {
+if (num === 10){
+console.log("good job")}{
+else if(num <= 10)
+console.log("not greater than 10")
+}}
 
