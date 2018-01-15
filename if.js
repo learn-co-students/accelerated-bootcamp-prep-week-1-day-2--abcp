b7d11529de4b46c31b3c5915da84daecc407b2d8
Mario Rodrigues <mariorodjr92@gmail.com>
2018-01-15 01:56:34 +0000

    Done.

--- if.js
+++ if.js
@@ -4,5 +4,10 @@ if (pokemon == "Pikachu") {
   console.log("I choose you Pikachu!!")
 }
 
-
-
+function isNumberGreaterThan(number) {
+  if (number > 10) {
+    return number
+  } else {
+    return "Sorry, that number is not 10!"
+  }
+}
