69b8502f33ea3be8c128e95bca093af9ee8bf590
A Lee <ayslee@gmail.com>
2018-01-16 22:51:14 +0000

    Done.

--- if.js
+++ if.js
@@ -4,5 +4,20 @@ if (pokemon == "Pikachu") {
   console.log("I choose you Pikachu!!")
 }
 
+function someFunction (num) {
+  if (num > 10) {
+    return num;
+  } else {
+    return "Sorry that number isn't greater than 10!";
+  }
+}
+console.log(someFunction(5));
 
+var burgers = ["Bob", "Linda", "Tina", "Louise", "Gene"];
+function printNames (array) {
+  for(i = 0; i < array.length; i++) {
+    console.log(burgers[i]);
+  }
+}
 
+printNames(burgers);
