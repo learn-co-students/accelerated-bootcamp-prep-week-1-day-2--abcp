0cceeb9d6c906c3a9c8d35dbf755585e503df431
Azumi Callanan <azucm@hotmail.com>
2018-01-16 08:35:18 +0000

    Done.

--- looping.js
+++ looping.js
@@ -4,8 +4,8 @@ console.log(names[0])
 console.log(names[1])
 console.log(names[2])
 
-for(i = 0; i < names.length; i++, ) {
-  console.log(name)
+for(i = 0; i < names.length; i++) {
+  console.log(name[i]);
 }
 
 
