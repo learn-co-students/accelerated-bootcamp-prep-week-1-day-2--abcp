bb7f400db70815ec1f34e35ad52280281a67a701
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-01-17 19:17:08 +0000

    Done.

--- array.js
+++ array.js
@@ -1,34 +1,47 @@
 function addElementToBeginningOfArray(array, element) {
- // add the element to the beginning of the array without mutating the array
+ var newArray = [element, ...array];
+ return newArray;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  // add element to the beginning of the array by mutating the array
+  array.unshift(element);
+  return array;
 }
 
 function addElementToEndOfArray(array, element) {
-
+  var bigArray = [...array, element];
+  return bigArray;
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+  array.push(element);
+  return array;
 }
 
 function accessElementInArray(array, index) {
-
+  return array[index];
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-
+  array.shift();
+  return array;
 }
 
 function removeElementFromBeginningOfArray(array) {
+  array.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-
+  array.pop();
+  return array;
 }
 
 function removeElementFromEndOfArray(array) {
+  var lastIndexItem = array.length-1;
+  array.slice(0, lastIndexItem);
+}
 
+function removeAtIndex(array, index) {
+  var new editedArray = [array.slice(o,index), array.slice(index+1)];
+  return editedArray;
 }
--- if.js
+++ if.js
@@ -4,5 +4,13 @@ if (pokemon == "Pikachu") {
   console.log("I choose you Pikachu!!")
 }
 
-
+function greaterThanTen(number) {
+  if (number>10) {
+    return number;
+  }
+  else {
+    return 'Sorry, the number is not greater than 10.'
+  }
+}
+  
 
