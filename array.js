b8e17be3ac2d81b780a8041fb141c2c9d9de8508
will stephens <wssdesigns@gmail.com>
2018-01-18 17:41:59 +0000

    Done.

--- array.js
+++ array.js
@@ -1,34 +1,39 @@
 function addElementToBeginningOfArray(array, element) {
+  return [element, ...aray]
  // add the element to the beginning of the array without mutating the array
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element)
   // add element to the beginning of the array by mutating the array
 }
 
 function addElementToEndOfArray(array, element) {
-
+  return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+  return array.push(element)
 }
 
 function accessElementInArray(array, index) {
-
+  array[index]
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-
+  return array.push(element)
+//
 }
 
 function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-
+  array.pop(element)
+  return array
 }
 
 function removeElementFromEndOfArray(array) {
-
+  return array.slice(0, array.length -1)
 }
