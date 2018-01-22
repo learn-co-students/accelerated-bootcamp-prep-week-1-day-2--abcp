a564400ccb53aa6bd9ad499e480ce64908fd1168
Rick Battipede <rbattipede@gmail.com>
2018-01-22 14:48:37 +0000

    Done.

--- if.js
+++ if.js
@@ -4,5 +4,14 @@ if (pokemon == "Pikachu") {
   console.log("I choose you Pikachu!!")
 }
 
+var number = 20
+
+function numberTest(number){
+  if(number >= 10){
+  return number;
+}
+else {
+  console.log("Sludge Dragon")
+}}
 
 
